849cd158c51c59bcf76b7a67b12ebc2c53cc32c9
andieljm <90933472+andieljm@users.noreply.github.com>
2022-08-11 01:37:02 -0600

    jasper tarea

--- src/main/java/com/Tienda/controller/ClienteController.java
+++ src/main/java/com/Tienda/controller/ClienteController.java
@@ -5,8 +5,14 @@
 package com.Tienda.controller;
 
 import com.Tienda.domain.Cliente;
+import com.Tienda.service.ClienteReportService;
 import com.Tienda.service.ClienteService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -24,19 +30,19 @@ public class ClienteController {
 
     @GetMapping("/cliente/listado")
     public String inicio(Model model) {
-        var clientes=clienteService.getClientes();
-        
-        var limiteTotal=0;
-        for (var c: clientes) {
-            limiteTotal+=c.getCredito().getLimite();
+        var clientes = clienteService.getClientes();
+
+        var limiteTotal = 0;
+        for (var c : clientes) {
+            limiteTotal += c.getCredito().getLimite();
         }
-        model.addAttribute("limiteTotal",limiteTotal);
-        model.addAttribute("totalClientes",clientes.size());
-        
-        model.addAttribute("clientes",clientes);
+        model.addAttribute("limiteTotal", limiteTotal);
+        model.addAttribute("totalClientes", clientes.size());
+
+        model.addAttribute("clientes", clientes);
         return "/cliente/listado";
     }
-    
+
     @GetMapping("/cliente/nuevo")
     public String nuevoCliente(Cliente cliente) {
         return "/cliente/modificar";
@@ -59,6 +65,27 @@ public class ClienteController {
     public String eliminarCliente(Cliente cliente) {
         clienteService.delete(cliente);
         return "redirect:/cliente/listado";
+    }
+
+    @Autowired
+    private ClienteReportService clienteReportService;
+
+    @GetMapping(value = "/cliente/ReporteClientes", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(clienteReportService.generateReport()));
+            byte[] targetArray = new byte[fis.available()];
+            fis.read(targetArray);
+            return targetArray;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
